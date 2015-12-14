447a77685711dc89284724cd201b7dc2a3f7dbeb
abaicus <baicusandrei@me.com>
2015-12-14 13:04:38 +0200

    Closed #6. Added Upsells in customizer

--- functions.php
+++ functions.php
@@ -213,73 +213,17 @@ function flymag_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'flymag_scripts' );
 
-/* tgm-plugin-activation */
-require_once get_template_directory() . '/class-tgm-plugin-activation.php';
 
 /**
- * TGMPA register
- */
-function flymag_register_required_plugins() {
-		$plugins = array(
+* Enqueue customizer custom style
+*/
+function flymag_customizer_custom_css() {
 
-			array(
-				'name'      => 'WP Product Reviews',
-				'slug'      => 'wp-product-reviews',
-				'required'  => false,
-			),
-
-			array(
-					'name'      => 'Login customizer',
-					'slug'      => 'login-customizer',
-					'required'  => false,
-			),
-
-			array(
-				'name'      => 'Intergeo Maps - Google Maps Plugin',
-				'slug'      => 'intergeo-maps',
-				'required'  => false
-			),
-
-			array(
-				'name'     => 'Pirate Forms',
-				'slug' 	   => 'pirate-forms',
-				'required' => false
-			));
-
-	$config = array(
-        'default_path' => '',
-        'menu'         => 'tgmpa-install-plugins',
-        'has_notices'  => true,
-        'dismissable'  => true,
-        'dismiss_msg'  => '',
-        'is_automatic' => false,
-        'message'      => '',
-        'strings'      => array(
-            'page_title'                      => esc_html__( 'Install Required Plugins', 'flymag' ),
-            'menu_title'                      => esc_html__( 'Install Plugins', 'flymag' ),
-            'installing'                      => esc_html__( 'Installing Plugin: %s', 'flymag' ),
-            'oops'                            => esc_html__( 'Something went wrong with the plugin API.', 'flymag' ),
-            'notice_can_install_required'     => _n_noop( 'This theme requires the following plugin: %1$s.', 'This theme requires the following plugins: %1$s.', 'flymag' ),
-            'notice_can_install_recommended'  => _n_noop( 'This theme recommends the following plugin: %1$s.', 'This theme recommends the following plugins: %1$s.', 'flymag' ),
-            'notice_cannot_install'           => _n_noop( 'Sorry, but you do not have the correct permissions to install the %s plugin. Contact the administrator of this site for help on getting the plugin installed.', 'Sorry, but you do not have the correct permissions to install the %s plugins. Contact the administrator of this site for help on getting the plugins installed.', 'flymag' ),
-            'notice_can_activate_required'    => _n_noop( 'The following required plugin is currently inactive: %1$s.', 'The following required plugins are currently inactive: %1$s.', 'flymag' ),
-            'notice_can_activate_recommended' => _n_noop( 'The following recommended plugin is currently inactive: %1$s.', 'The following recommended plugins are currently inactive: %1$s.', 'flymag' ),
-            'notice_cannot_activate'          => _n_noop( 'Sorry, but you do not have the correct permissions to activate the %s plugin. Contact the administrator of this site for help on getting the plugin activated.', 'Sorry, but you do not have the correct permissions to activate the %s plugins. Contact the administrator of this site for help on getting the plugins activated.', 'flymag' ),
-            'notice_ask_to_update'            => _n_noop( 'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.', 'flymag' ),
-            'notice_cannot_update'            => _n_noop( 'Sorry, but you do not have the correct permissions to update the %s plugin. Contact the administrator of this site for help on getting the plugin updated.', 'Sorry, but you do not have the correct permissions to update the %s plugins. Contact the administrator of this site for help on getting the plugins updated.', 'flymag' ),
-            'install_link'                    => _n_noop( 'Begin installing plugin', 'Begin installing plugins', 'flymag' ),
-            'activate_link'                   => _n_noop( 'Begin activating plugin', 'Begin activating plugins', 'flymag' ),
-            'return'                          => esc_html__( 'Return to Required Plugins Installer', 'flymag' ),
-            'plugin_activated'                => esc_html__( 'Plugin activated successfully.', 'flymag' ),
-            'complete'                        => esc_html__( 'All plugins installed and activated successfully. %s', 'flymag' ),
-            'nag_type'                        => 'updated'
-        )
-    );
-
-	tgmpa( $plugins, $config );
+    wp_enqueue_style('flymag_customizer_custom_css', get_template_directory_uri() . '/css/flymag_customizer_style.css');
 
 }
-add_action( 'tgmpa_register', 'flymag_register_required_plugins' );
+add_action('customize_controls_print_styles', 'flymag_customizer_custom_css');
+
 
 /**
  * Set custom classes for the top menu items.
