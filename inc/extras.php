86f9e40124e26bf26374c92b44bc1002263c38e2
poonam9 <poonamdev27@gmail.com>
2017-04-18 16:30:46 +0530

    #38
    Woocommerce activation query

--- inc/extras.php
+++ inc/extras.php
@@ -102,3 +102,27 @@ function flymag_setup_author() {
 	}
 }
 add_action( 'wp', 'flymag_setup_author' );
+if ( ! function_exists( 'flymag_is_woocommerce_activated' ) ) {
+	/**
+	 * Query WooCommerce activation
+	 */
+	function flymag_is_woocommerce_activated() {
+		return class_exists( 'woocommerce' ) ? true : false;
+	}
+}
+
+/**
+ * Checks if the current page is a product archive
+ * @return boolean
+ */
+function flymag_is_product_archive() {
+	if ( flymag_is_woocommerce_activated() ) {
+		if ( is_shop() || is_product_taxonomy() || is_product_category() || is_product_tag() ) {
+			return true;
+		} else {
+			return false;
+		}
+	} else {
+		return false;
+	}
+}
